d7eee020ab60ecc43d6b20a706ac387ee0b7f658
Dushusir <1414556676@qq.com>
2025-06-20 11:26:26 +0800

    fix: domain path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/prompt-gallery/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
